48c90154ab949cdbf71e807f8f7a7d377df0be18
Jeremy Darling <jeremy.darling@pearson.com>
2014-09-23 11:47:21 -0500

    Add PinFilter controller

---
+++ examples/web/web/src/js/controllers/pinfilter.js
@@ -0,0 +1,52 @@
+var controllers = require('../../lib/controllers.js');
+var handlebarsHelpers = require('../../lib/handlebarsHelpers');
+var support = require('../../lib/support');
+var Loader = require('../../lib/loader');
+var els = support.els;
+var el = support.el;
+
+var PinFilterController = function(container, data){
+  var self = this;
+  var filterList = function(src){
+    var re = new RegExp(src, 'gi');
+    var boxes = els(container.parentNode, '[data-pins]');
+    boxes.forEach(function(box){
+      var pins = box.dataset.pins.split(',').map(function(s){
+        return s.trim();
+      }).join('\n');
+      box.className = box.className.replace(/(^|\W+)hidden(\W+|$)/gi, '').trim();
+      if(src && !pins.match(re)){
+        box.className = box.className + ' hidden';
+      }
+    });
+  };
+  var clickHandler = self.clickHandler = function(e){
+    if(e.target && e.target.nodeName === 'BUTTON'){
+      var val = support.val(el(container, '[role="search"] input'));
+      filterList(val);
+      e.preventDefault();
+      return false;
+    }
+  };
+  var keyHandler = self.keyHandler = function(e){
+    var code = e.key||e.keyCode;
+    if(!e.target){
+      return;
+    }
+    filterList(support.val(e.target));
+    if(code === 13){
+      e.preventDefault();
+      return false;
+    }
+  };
+  container.addEventListener('click', clickHandler);
+  container.addEventListener('keyup', keyHandler);
+};
+
+PinFilterController.teardown = function(container){
+  var self = this;
+  container.removeEventListener('click', self.clickHandler);
+  container.removeEventListener('keyup', self.keyHandler);
+};
+
+controllers.register('PinFilter', PinFilterController);
