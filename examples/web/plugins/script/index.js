c9d8a25262a839b27ab4b91273766872626a696d
Jeremy Darling <jeremy.darling@pearson.com>
2014-09-23 22:20:22 -0500

    Initial script activate in place, not wired in

--- examples/web/plugins/script/index.js
+++ examples/web/plugins/script/index.js
@@ -1,6 +1,16 @@
 var fs = require('fs');
+var vm = require('vm');
+var rock = require('../../lib/rock');
+var CONST = require('../../../../index').CONST;
 
-var source = '// Put your source here';
+var source = '// Put your source here\n\n'+
+      'rock.setPinMode(J12_P37, \'output\', function(){\n'+
+      '  rock.set(J12_P37, 1, function(){\n'+
+      '    setTimeout(function(){\n'+
+      '      rock.set(J12_P37, 0);\n'+
+      '    }, 1000);\n'+
+      '  });\n'+
+      '});\n';
 
 var getScript = function(req, reply){
   fs.readFile('./script.js', function(err, script){
@@ -25,6 +35,30 @@ var updateScript = function(req, reply){
   });
 };
 
+var activateScript = function(req, reply){
+  var source = req.payload;
+  var sandbox = {
+    rock: rock,
+    setTimeout: setTimeout
+  };
+  var keys = Object.keys(CONST);
+  keys.forEach(function(key){
+    sandbox[key] = CONST[key];
+  });
+  try{
+    var f = new Function(source);
+    reply({
+      root: 'result',
+      result: vm.runInNewContext(source, sandbox)
+    });
+  }catch(e){
+    reply({
+      root: 'error',
+      error: e.stack||e
+    });
+  }
+};
+
 module.exports = function(options, next){
   var config = options.config;
   var server = options.hapi;
@@ -44,6 +78,11 @@ module.exports = function(options, next){
       path: config.route+(config.path||'script'),
       handler: updateScript
     },
+    {
+      method: 'POST',
+      path: config.route+(config.path||'script/activate'),
+      handler: activateScript
+    }
   ]);
   next();
 };
